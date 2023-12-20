e1388ddf71af3466bb4841bacea9b66ed1c4da29
yerim <kom5327@naver.com>
2023-12-20 22:14:22 +0900

    About components

---
+++ src/views/DataBinding.vue
@@ -0,0 +1,28 @@
+<template>
+    <label for="">
+        <input type="checkbox" value="서울" v-model="checked">{{ checked }}
+    </label>
+
+    <label for="">
+        <input type="checkbox" value="부산" v-model="checked">{{ checked }}
+    </label>
+
+    <label for="">
+        <input type="checkbox" value="광명" v-model="checked">{{ checked }}
+    </label>
+
+    <br>
+
+    <span>체크한 지역 : {{ checked }}</span>
+</template>
+<script>
+
+export default {
+  name: 'DataBinding',
+  data () {
+    return {
+        checked: []
+  };
+},
+}
+</script>
